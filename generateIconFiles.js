d5909c750869e66cd7f90eeb7f6fd3578ba1ab4f
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-08-12 03:32:01 +0000

    fix: Use camelCase in generate Icon funciton

--- generateIconFiles.js
+++ generateIconFiles.js
@@ -12,27 +12,29 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true })
 }
 
-// Função para converter de kebab-case para camelCase
-const toCamelCase = (str) => {
-  return str.replace(/-([a-z])/g, (match, p1) => p1.toUpperCase())
-}
+// Converte um nome de ícone de kebab-case para camelCase
+const toCamelCase = (str) =>
+  str.replace(/-./g, (match) => match.charAt(1).toUpperCase())
 
 // Função para gerar o conteúdo do componente para um ícone específico
-const generateIconComponent = (iconName, camelCaseName) => `
+const generateIconComponent = (iconName) => {
+  const camelCaseName = toCamelCase(iconName)
+
+  return `
 import React from 'react'
 import { Icon, IconProps } from '../Icon'
 
 export function ${camelCaseName}Icon({ iconName = "${iconName}", ...props }: IconProps) {
   return <Icon {...props} iconName={iconName} />
 }
-`
+`.trim()
+}
 
 // Gera e escreve um arquivo para cada ícone
 iconNames.forEach((iconName) => {
-  const camelCaseName = toCamelCase(iconName)
-  const fileContent = generateIconComponent(iconName, camelCaseName)
-  const filePath = path.join(outputDir, `${camelCaseName}Icon.tsx`)
-  fs.writeFileSync(filePath, fileContent.trim(), 'utf8')
+  const fileContent = generateIconComponent(iconName)
+  const filePath = path.join(outputDir, `${toCamelCase(iconName)}Icon.tsx`)
+  fs.writeFileSync(filePath, fileContent, 'utf8')
   console.log(`Arquivo criado: ${filePath}`)
 })
 
