8c6cff138ac0a8c488f9b4af78cb3ae23a00970d
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-09-03 23:14:59 +0000

    ci: rmv icon

--- src/icons/AdvertisementIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Icon, IconProps } from '../Icon'
-
-export function AdvertisementIcon({
-  iconName = 'advertisement',
-  ...props
-}: IconProps) {
-  return <Icon {...props} iconName={iconName} />
-}
